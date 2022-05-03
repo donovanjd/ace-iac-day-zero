61712f6571dc3c55f9c567316c100edd6d127a3c
Donovan Durand <donovandurand@gmail.com>
2022-05-03 22:50:32 +1000

    Update versions.tf
    Change aviatrix provider version to 2.20.3

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 2.21.2"
+      version = "~> 2.20.3"
     }
     aws = {
       source  = "hashicorp/aws"
